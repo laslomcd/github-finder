1daf803a70d97e316b0d5ecd3d66d5d392ed892d
Ryan McDonagh <ryan@henschen.com>
2020-02-27 13:43:14 -0500

    started project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 
+import Navbar from './components/layout/Navbar.component';
+import UserItem from './components/Users/UserItem.component';
+
 import "./App.css";
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello From React</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Navbar />
+                <UserItem />
+            </div>
+        );
+    }
 }
 
 export default App;
