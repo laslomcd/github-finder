f6b893e970190ee2ce1fe58cd38a8463d6ee9a6f
ryan <laslomcd@gmail.com>
2020-04-01 16:55:51 -0400

    add ability to search single users

--- src/App.js
+++ src/App.js
@@ -1,47 +1,110 @@
-import React, { Component } from "react";
-import axios from 'axios';
+import React, { Component, Fragment } from "react";
+import { Switch, Route } from "react-router-dom";
+import axios from "axios";
 
-import Navbar from './components/layout/Navbar.component';
-import Users from './components/Users/Users.component';
-import Search from './components/Users/Search.component';
+import Navbar from "./components/layout/Navbar.component";
+import Users from "./components/Users/Users.component";
+import User from "./components/Users/User.component";
+import Search from "./components/Users/Search.component";
+import About from "./components/Pages/About.component";
+import Alert from "./components/layout/Alert.component";
 
 import "./App.css";
 
 class App extends Component {
-    state = {
-        users: [],
-        loading: false
-    }
-    // async componentDidMount() {
-    //     this.setState({ loading: true });
-    //     const response = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-    //     this.setState({ users: response.data, loading: false });
+  state = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+    alert: null
+  };
+  // async componentDidMount() {
+  //     this.setState({ loading: true });
+  //     const response = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+  //     this.setState({ users: response.data, loading: false });
 
-    // }
+  // }
+  // Search Github Users
+  searchUsers = async text => {
+    this.setState({ loading: true });
+    const response = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: response.data.items, loading: false });
+  };
 
-    searchUsers = async text => {
-        this.setState({ loading: true })
-        const response = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-        this.setState({ users: response.data.items, loading: false });
-    }
+  // Get Single User
+  getUser = async username => {
+    this.setState({ loading: true });
+    const response = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ user: response.data, loading: false });
+  };
 
-    clearUsers = () => {
-        this.setState({ users: [], loading: false });
-    }
+  // Get User Repos
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+    const response = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ repos: response.data, loading: false });
+  };
 
-    render() {
-        const { users, loading } = this.state;
-        return (
-            <div className="App">
-                <Navbar />
-                <div className="container">
-                    <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true : false} />
-                    <Users loading={loading} users={users} />
-                </div>
+  // Clear Users from State
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
 
-            </div>
-        );
-    }
+  setAlert = (message, type) => {
+    this.setState({ alert: { message: message, type: type } });
+    setTimeout(() => this.setState({ alert: null }), 3000);
+  };
+
+  render() {
+    const { users, user, loading, repos } = this.state;
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Alert alert={this.state.alert} />
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <Fragment>
+                  <Search
+                    searchUsers={this.searchUsers}
+                    clearUsers={this.clearUsers}
+                    showClear={users.length > 0 ? true : false}
+                    setAlert={this.setAlert}
+                  />
+                  <Users loading={loading} users={users} />
+                </Fragment>
+              )}
+            />
+            <Route exact path="/about" render={props => <About {...props} />} />
+            <Route
+              exact
+              path="/user/:login"
+              render={props => (
+                <User
+                  {...props}
+                  getUser={this.getUser}
+                  getUserRepos={this.getUserRepos}
+                  user={user}
+                  repos={repos}
+                  loading={loading}
+                />
+              )}
+            />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Users/Search.component.jsx
+++ src/components/Users/Search.component.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Search extends Component {
     state = {
-        text: ''
+        text: '',
     }
 
     onChange = (e) => {
@@ -11,8 +11,13 @@ class Search extends Component {
 
     onSubmit = (e) => {
         e.preventDefault();
-        this.props.searchUsers(this.state.text);
-        this.setState({ text: '' });
+        if(this.state.text === '') {
+            this.props.setAlert('Please enter something', 'light')
+        } else {
+            this.props.searchUsers(this.state.text);
+            this.setState({ text: '' });
+        }
+        
     }
 
     render() {
